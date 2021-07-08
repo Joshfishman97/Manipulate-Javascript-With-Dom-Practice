d0dfcdcbb7956af2875b5db85bf4b5af64df0bb2
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-08 20:20:56 +0000

    another commit

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -1,6 +1,10 @@
 var elem = document.createElement("h1");
 elem.innerHTML = "Hello World";
 document.querySelector("#myDiv").appendChild(elem);
-
+var pelem = document.createElement("p")
+var text = document.createTextNode("Hello World")
+pelem.innerHTML = "text"
+pelem.style.background = "yellow"
+document.querySelector('#myDiv').appendChild(pelem)
 //Your code here
 // Don'change the code above
