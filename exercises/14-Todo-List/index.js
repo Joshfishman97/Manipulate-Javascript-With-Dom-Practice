9c78fcae89ae388790333bed58afbf833599ab65
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-10 17:29:13 +0000

    commit

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -6,18 +6,21 @@ todo.addEventListener("keydown", function(event) {
 		let li = document.createElement("li");
 		li.innerHTML = `<span><i class="fa fa-trash"></i></span> ${todo.value}`;
 		ul.appendChild(li);
+		let trashCan = li.querySelector(".fa.fa-trash");
 		todo.value = "";
+		addListener(trashCan);
 	}
 });
 
-let del = document.querySelectorAll(".fa.fa-trash");
-del.forEach(item => {
-	item.addEventListener("click", function(event) {
-		let ul = document.querySelector("ul");;
+let allTrashCans = document.querySelectorAll(".fa.fa-trash");
+allTrashCans.forEach(item => {
+	addListener(item);
+});
+//the event looked at in the add lisnter above is the <i> tag
+function addListener(domElement) {
+	domElement.addEventListener("click", function(event) {
+		let ul = document.querySelector("ul");
 		let li = this.parentNode.parentNode;
 		ul.removeChild(li);
 	});
-});
-//the event looked at in the add lisnter above is the <i> tag
-
-
+}
